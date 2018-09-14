0514134c75f65ab13d6db499b9851d3f43046a24
w-mj <w.mingjian@yahoo.com>
2018-09-14 22:58:15 +0800

    vga success on verilog

---
+++ VGADriver.v
@@ -0,0 +1,177 @@
+/////////////////////////////////////////////////////////////////////////////
+//Altera ATPP合作伙伴 至芯科技 携手 特权同学 共同打造 FPGA开发板系列
+//工程硬件平台： Altera Cyclone IV FPGA 
+//开发套件型号： SF-CY4 特权打造
+//版   权  申   明： 本例程由《深入浅出玩转FPGA》作者“特权同学”原创，
+//				仅供SF-CY4开发套件学习使用，谢谢支持
+//官方淘宝店铺： http://myfpga.taobao.com/
+//最新资料下载： http://pan.baidu.com/s/1jGpMIJc
+//公                司： 上海或与电子科技有限公司
+/////////////////////////////////////////////////////////////////////////////
+module vga_controller(
+			input clk_25m,	//PLL输出25MHz时钟
+			input rst_n,	//复位信号，低电平有效
+			input pixel_r, pixel_g, pixel_b,
+			output[11: 0] row, colum, 
+			output vga_r,	//VGA显示色彩R
+			output vga_g,	//VGA显示色彩G
+			output vga_b,	//VGA显示色彩B
+			output reg vga_hsy,	//VGA显示行同步信号
+			output reg vga_vsy	//VGA显示场同步信号
+		);
+
+//-----------------------------------------------------------
+wire clk;
+
+
+//-----------------------------------------------------------
+//VGA Timing 640*480 & 25MHz & 60Hz
+assign clk = clk_25m;
+	
+parameter VGA_HTT = 12'd800-12'd1;	//Hor Total Time
+parameter VGA_HST = 12'd96;		//Hor Sync  Time
+parameter VGA_HBP = 12'd48;//+12'd16;		//Hor Back Porch
+parameter VGA_HVT = 12'd640;	//Hor Valid Time
+parameter VGA_HFP = 12'd16;		//Hor Front Porch
+
+parameter VGA_VTT = 12'd525-12'd1;	//Ver Total Time
+parameter VGA_VST = 12'd2;		//Ver Sync Time
+parameter VGA_VBP = 12'd33;//-12'd4;		//Ver Back Porch
+parameter VGA_VVT = 12'd480;	//Ver Valid Time
+parameter VGA_VFP = 12'd10;		//Ver Front Porch
+
+parameter VGA_CORBER = 12'd80;	//8等分做Color bar显示
+
+//-----------------------------------------------------------
+	//x和y坐标计数器
+reg[11:0] xcnt;
+reg[11:0] ycnt;
+	
+always @(posedge clk or negedge rst_n)
+	if(!rst_n) xcnt <= 12'd0;
+	else if(xcnt >= VGA_HTT) xcnt <= 12'd0;
+	else xcnt <= xcnt+1'b1;
+
+always @(posedge clk or negedge rst_n)
+	if(!rst_n) ycnt <= 12'd0;
+	else if(xcnt == VGA_HTT) begin
+		if(ycnt >= VGA_VTT) ycnt <= 12'd0;
+		else ycnt <= ycnt+1'b1;
+	end
+	else ;
+		
+//-----------------------------------------------------------
+	//行、场同步信号生成
+always @(posedge clk or negedge rst_n)
+	if(!rst_n) vga_hsy <= 1'b0;
+	else if(xcnt < VGA_HST) vga_hsy <= 1'b1;
+	else vga_hsy <= 1'b0;
+	
+always @(posedge clk or negedge rst_n)
+	if(!rst_n) vga_vsy <= 1'b0;
+	else if(ycnt < VGA_VST) vga_vsy <= 1'b1;
+	else vga_vsy <= 1'b0;	
+	
+//-----------------------------------------------------------	
+	//显示有效区域标志信号生成
+reg vga_valid;	//显示区域内，该信号高电平
+
+reg[11: 0] row_r, colum_r;
+assign row = row_r;
+assign colum = colum_r;
+
+always @(posedge clk or negedge rst_n)
+	if(!rst_n) vga_valid <= 1'b0;
+	else 
+		if((xcnt >= (VGA_HST+VGA_HBP)) && (xcnt < (VGA_HST+VGA_HBP+VGA_HVT))
+				&& (ycnt >= (VGA_VST+VGA_VBP)) && (ycnt < (VGA_VST+VGA_VBP+VGA_VVT))) begin
+		 vga_valid <= 1'b1;
+		 colum_r = (xcnt - (VGA_HST+VGA_HBP));
+		 row_r = (ycnt - (VGA_VST+VGA_VBP));
+		 end
+	else vga_valid <= 1'b0;
+	
+////-----------------------------------------------------------
+//	//显示色彩生产逻辑
+//reg vga_rdb;	//R色彩
+//reg vga_gdb;	//G色彩
+//reg vga_bdb;	//B色彩
+//
+//always @(posedge clk or negedge rst_n)
+//	if(!rst_n) begin
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b0;
+//		vga_bdb <= 1'b0;
+//	end
+//	else if(xcnt == (VGA_HST+VGA_HBP)) begin	//显示第一行为绿色
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b0;		
+//	end
+//	else if(xcnt == (VGA_HST+VGA_HBP+VGA_HVT-1'b1)) begin	//显示最后一行为绿色
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b0;		
+//	end
+//	else if(ycnt == (VGA_VST+VGA_VBP)) begin	//显示第一列为绿色
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b0;			
+//	end
+//	else if(ycnt == (VGA_VST+VGA_VBP+VGA_VVT-1'b1)) begin	//显示最后一列为绿色
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b0;		
+//	end
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER)) begin		//显示第1个Color bar
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b0;
+//		vga_bdb <= 1'b0;	
+//	end
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER)) begin		//显示第2个Color bar
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b0;
+//		vga_bdb <= 1'b1;
+//	end	
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER+VGA_CORBER)) begin		//显示第3个Color bar
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b0;
+//	end	
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER)) begin		//显示第4个Color bar
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b1;
+//	end	
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER)) begin		//显示第5个Color bar
+//		vga_rdb <= 1'b1;
+//		vga_gdb <= 1'b0;
+//		vga_bdb <= 1'b0;	
+//	end	
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER)) begin		//显示第6个Color bar
+//		vga_rdb <= 1'b1;
+//		vga_gdb <= 1'b0;
+//		vga_bdb <= 1'b1;	
+//	end
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER)) begin		//显示第7个Color bar
+//		vga_rdb <= 1'b1;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b0;	
+//	end
+//	else if(xcnt <= (VGA_HST+VGA_HBP+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER+VGA_CORBER)) begin		//显示第8个Color bar
+//		vga_rdb <= 1'b1;
+//		vga_gdb <= 1'b1;
+//		vga_bdb <= 1'b1;	
+//	end
+//	else begin
+//		vga_rdb <= 1'b0;
+//		vga_gdb <= 1'b0;
+//		vga_bdb <= 1'b0;	
+//	end
+
+assign vga_r = vga_valid ? pixel_r:1'b0;
+assign vga_g = vga_valid ? pixel_g:1'b0;	
+assign vga_b = vga_valid ? pixel_b:1'b0;	
+	
+endmodule
+
--- vga.vhd
+++ vga.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.NUMERIC_STD.all;
+use ieee.NUMERIC_STD.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 -- 640*480@60HZ
 -- 80*25 characters, 8*16 for one character.
@@ -30,7 +32,8 @@ mod_addr <= mem_data(6 downto 0) & std_logic_vector(to_unsigned(char_v_cnt, 4));
 g <= mod_data(char_h_cnt);
 
 process (clk) begin
-	if (v_cnt < 35 or v_cnt >= 515) then 
+	if (clk'event and clk = '1') then 
+	if (v_cnt < 34 or v_cnt >= 514) then 
 		if (v_cnt < 2) then
 			vs <= '0';
 		else 
@@ -42,8 +45,8 @@ process (clk) begin
 		end if;
 		
 	else 
-		if (h_cnt < 144 or h_cnt > 784) then
-			if (h_cnt < 96) then
+		if (h_cnt < 138 or h_cnt >= 778) then
+			if (h_cnt < 93) then
 				hs <= '0';
 			else 
 				hs <= '1';
@@ -73,5 +76,158 @@ process (clk) begin
 			end if;
 		end if;
 	end if;
+	end if;
 end process;
 end;
+
+Library IEEE;
+use IEEE.STD_Logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+entity VGAdrive is
+  port( clock            : in std_logic;  -- 25.175 Mhz clock
+        red, green, blue : in std_logic;  -- input values for RGB signals
+        row, column : out std_logic_vector(9 downto 0); -- for current pixel
+        Rout, Gout, Bout, H, V : out std_logic); -- VGA drive signals
+  -- The signals Rout, Gout, Bout, H and V are output to the monitor.
+  -- The row and column outputs are used to know when to assert red,
+  -- green and blue to color the current pixel.  For VGA, the column
+  -- values that are valid are from 0 to 639, all other values should
+  -- be ignored.  The row values that are valid are from 0 to 479 and
+  -- again, all other values are ignored.  To turn on a pixel on the
+  -- VGA monitor, some combination of red, green and blue should be
+  -- asserted before the rising edge of the clock.  Objects which are
+  -- displayed on the monitor, assert their combination of red, green and
+  -- blue when they detect the row and column values are within their
+  -- range.  For multiple objects sharing a screen, they must be combined
+  -- using logic to create single red, green, and blue signals.
+end;
+
+architecture behaviour1 of VGAdrive is
+  -- names are referenced from Altera University Program Design
+  -- Laboratory Package  November 1997, ver. 1.1  User Guide Supplement
+  -- clock period = 39.72 ns; the constants are integer multiples of the
+  -- clock frequency and are close but not exact
+  -- row counter will go from 0 to 524; column counter from 0 to 799
+  subtype counter is std_logic_vector(9 downto 0);
+  constant B : natural := 93;  -- horizontal blank: 3.77 us
+  constant C : natural := 45;  -- front guard: 1.89 us
+  constant D : natural := 640; -- horizontal columns: 25.17 us
+  constant E : natural := 22;  -- rear guard: 0.94 us
+  constant A : natural := B + C + D + E;  -- one horizontal sync cycle: 31.77 us
+  constant P : natural := 2;   -- vertical blank: 64 us
+  constant Q : natural := 32;  -- front guard: 1.02 ms
+  constant R : natural := 480; -- vertical rows: 15.25 ms
+  constant S : natural := 11;  -- rear guard: 0.35 ms
+  constant O : natural := P + Q + R + S;  -- one vertical sync cycle: 16.6 ms
+   
+begin
+
+  Rout <= red;
+  Gout <= green;
+  Bout <= blue;
+
+  process
+    variable vertical, horizontal : counter;  -- define counters
+  begin
+    wait until clock = '1';
+
+  -- increment counters
+      if  horizontal < A - 1  then
+        horizontal := horizontal + 1;
+      else
+        horizontal := (others => '0');
+
+        if  vertical < O - 1  then -- less than oh
+          vertical := vertical + 1;
+        else
+          vertical := (others => '0');       -- is set to zero
+        end if;
+      end if;
+
+  -- define H pulse
+      if  horizontal >= (D + E)  and  horizontal < (D + E + B)  then
+        H <= '0';
+      else
+        H <= '1';
+      end if;
+
+  -- define V pulse
+      if  vertical >= (R + S)  and  vertical < (R + S + P)  then
+        V <= '0';
+      else
+        V <= '1';
+      end if;
+
+    -- mapping of the variable to the signals
+     -- negative signs are because the conversion bits are reversed
+    row <= vertical;
+    column <= horizontal;
+
+  end process;
+
+end architecture;
+
+
+-- RGB VGA test pattern  Rob Chapman  Mar 9, 1998
+
+ -- This file uses the VGA driver and creates 3 squares on the screen which
+ -- show all the available colors from mixing red, green and blue
+Library IEEE;
+use IEEE.STD_Logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity vgatest is
+  port(clock         : in std_logic;
+       R, G, B, H, V : out std_logic);
+end entity;
+
+architecture test of vgatest is
+
+  component vgadrive is
+    port( clock          : in std_logic;  -- 25.175 Mhz clock
+        red, green, blue : in std_logic;  -- input values for RGB signals
+        row, column      : out std_logic_vector(9 downto 0); -- for current pixel
+        Rout, Gout, Bout, H, V : out std_logic); -- VGA drive signals
+  end component;
+  
+  signal row, column : std_logic_vector(9 downto 0);
+  signal red, green, blue : std_logic;
+
+begin
+
+  -- for debugging: to view the bit order
+  VGA : component vgadrive
+    port map ( clock => clock, red => red, green => green, blue => blue,
+               row => row, column => column,
+               Rout => R, Gout => G, Bout => B, H => H, V => V);
+ 
+  -- red square from 0,0 to 360, 350
+  -- green square from 0,250 to 360, 640
+  -- blue square from 120,150 to 480,500
+  RGB : process(row, column)
+  begin
+    -- wait until clock = '1';
+    
+    if  row < 360 and column < 350  then
+      red <= '1';
+    else
+      red <= '0';
+    end if;
+    
+    if  row < 360 and column > 250 and column < 640  then
+      green <= '1';
+    else
+      green <= '0';
+    end if;
+    
+    if  row > 120 and row < 480 and column > 150 and column < 500  then
+      blue <= '1';
+    else
+      blue <= '0';
+    end if;
+
+  end process;
+
+end architecture;
